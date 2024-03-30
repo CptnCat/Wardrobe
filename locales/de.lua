184effa2941f5e26ab2e3cfbcef3657f0b8d0781
EpicCat <78860645+CptnCat@users.noreply.github.com>
2024-03-30 03:31:56 +0100

    Add files via upload

---
+++ locales/de.lua
@@ -0,0 +1,23 @@
+Locales['de'] = {
+    ['interaction']  = 'Benutze ~INPUT_CONTEXT~, um auf deine Garderobe zuzugreifen',
+  -- Menu
+    ['wardrobe']  = 'Kleiderschrank',
+    ['my_outfits'] = 'Meine Outfits',
+    ['save_current_outfit'] = 'Aktuelles Outfit speichern',
+    ['outfit_interactions'] = 'Outfitinteratkionen',
+    ['put_outfit_on'] = 'Outfit anziehen',
+    ['share_outfit'] = 'Outfit teilen',
+    ['rename_outfit'] = 'Outfit umbenennen',
+    ['delete_outfit'] = 'Outfit löschen',
+  -- Input Dialog
+    ['name_the_outfit'] = 'Name des Outfits',
+    ['favorite_outfit'] = 'Als Favorit markieren',
+  -- Notifications
+    ['outfit_saved'] = 'Dein Outfit (~b~%s~s~) wurde gespeichert.',
+    ['no_outfits_in_wardrobe'] = '~r~Du hast keine Outfits im Kleiderschrank.',
+    ['no_player_nearby'] = '~r~Keine Person in deiner Nähe.',
+    ['outfit_shared'] = 'Du hast das Outift ~b~%s~s~ mit jemanden geteilt.',
+    ['favorite_removed'] = 'Du hast das Outift ~b~%s~s~ aus deinen ~y~Favoriten~s~ entfernt.',
+    ['favorite_added'] = 'Du hast das Outift ~b~%s~s~ zu deinen ~y~Favoriten~s~ hinzugefügt.',
+    ['outfit_deleted'] = 'Du hast das Outift ~r~%s~s~ gelöscht.',
+  }
---
+++ locales/en.lua
@@ -0,0 +1,23 @@
+Locales['en'] = {
+    ['interaction']  = 'Press ~INPUT_CONTEXT~, to interact with the wardrobe',
+  -- Menu
+    ['wardrobe']  = 'Wardrobe',
+    ['my_outfits'] = 'My Outfits',
+    ['save_current_outfit'] = 'Save current Outfit',
+    ['outfit_interactions'] = 'Outfitinteractions',
+    ['put_outfit_on'] = 'Put on Outfit',
+    ['share_outfit'] = 'Share Outfit',
+    ['rename_outfit'] = 'Rename Outift',
+    ['delete_outfit'] = 'Delete Outfit',
+  -- Input Dialog
+    ['name_the_outfit'] = 'Name the Outfit',
+    ['favorite_outfit'] = 'Mark as favorite',
+  -- Notifications
+    ['outfit_saved'] = 'Your Outfit (~b~%s~s~) has been saved.',
+    ['no_outfits_in_wardrobe'] = '~r~You don\'t have any outfits in your wardrobe.',
+    ['no_player_nearby'] = '~r~There is no one near you.',
+    ['outfit_shared'] = 'You shared the outfit ~b~%s~s~ with someone.',
+    ['favorite_removed'] = 'You removed the outfit ~b~%s~s~ from your ~y~favorites~s~.',
+    ['favorite_added'] = 'You added the outfit ~b~%s~s~ to your ~y~favorites~s~.',
+    ['outfit_deleted'] = 'You deleted the outfit ~r~%s~s~.',
+  }
