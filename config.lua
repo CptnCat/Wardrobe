e0c0364109db1b7ee1741507b1f86e371c2f8b22
EpicCat <78860645+CptnCat@users.noreply.github.com>
2024-04-22 15:17:16 +0200

    Updated config.lua
    Added vector3 functions and Config.EnableSaveCurrentOutfit

--- config.lua
+++ config.lua
@@ -13,20 +13,24 @@ Config.EnableAdminCommand = true -- Enable the command to open the wardrobe anyw
 Config.AdminCommand = 'wardrobe' -- Command to open the wardrobe
 Config.AdminGroups = 'admin' -- Add your admins here that should have access to the wardrobe command
 
+-- General
+Config.EnableSaveCurrentOutfit = true -- Enable the Save Current Outfit selection
+
 -- Locations
 Config.WardrobePositions = { -- Add more positions here
-    {x = 429.7014, y = -811.3698, z = 29.4911},
-    {x = 71.1883, y = -1387.8079, z = 29.3761},
-    {x = -820.0474, y = -1067.3770, z = 11.3281},
-    {x = -1182.0992, y = -765.1116, z = 17.3267},
-    {x = -1446.7981, y = -242.7872, z =  49.8227},
-    {x = -703.7148, y = -151.2709, z = 37.4151},
-    {x = -168.9946, y = -232.2905, z = 39.7333},
-    {x = 118.1551, y = -232.2905, z = 54.5578},
-    {x = -3178.6782, y = 1035.7823, z = 20.8632},
-    {x = -1100.2936, y = 2717.1482, z = 19.1078},
-    {x = 617.6683, y = 2773.5955, z = 42.0881},
-    {x = 1201.9600, y = 2714.4006, z = 38.2226},
-    {x = 1698.7163, y = 4818.1343, z = 42.0631},
-    {x = 3.8752, y = 6505.8486, z = 31.8778}
+    {coords = vec3(429.7014, -811.3698, 29.4911)},
+    {coords = vec3(71.1883, -1387.8079, 29.3761)},
+    {coords = vec3(-820.0474, -1067.3770, 11.3281)},
+    {coords = vec3(-1182.0992, -765.1116, 17.3267)},
+    {coords = vec3(-1446.7981, -242.7872,  49.8227)},
+    {coords = vec3(-703.7148, -151.2709, 37.4151)},
+    {coords = vec3(-168.9946, -232.2905, 39.7333)},
+    {coords = vec3(118.1551, -232.2905, 54.5578)},
+    {coords = vec3(-3178.6782, 1035.7823, 20.8632)},
+    {coords = vec3(-3178.6782, 1035.7823, 20.8632)},
+    {coords = vec3(-1100.2936, 2717.1482, 19.1078)},
+    {coords = vec3(617.6683, 2773.5955, 42.0881)},
+    {coords = vec3(1201.9600, 2714.4006, 38.2226)},
+    {coords = vec3(1698.7163, 4818.1343, 42.0631)},
+    {coords = vec3(3.8752, 6505.8486, 31.8778)}
 }
